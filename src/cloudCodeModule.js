4b4f02dacc1171e99c29ff30fff0d8d3e71a83a1
Reilly Knutson <67723956+Reillyknut@users.noreply.github.com>
2022-03-21 13:54:14 -0400

    run command should be a POST request

--- src/cloudCodeModule.js
+++ src/cloudCodeModule.js
@@ -1,6 +1,6 @@
 const cloudCodeModule = (sendRequest) => {
   const run = (ccfName) => {
-    return sendRequest(`/ccf/${ccfName}/run`, 'GET');
+    return sendRequest(`/ccf/${ccfName}/run`, 'POST');
   };
 
   return {
