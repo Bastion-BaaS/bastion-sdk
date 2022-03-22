5fe2f5a37bcca899be0275950a00b2b18e88ab1e
Reilly Knutson <Reillyknut@gmail.com>
2022-03-22 13:49:58 -0400

    Using correct routes now. But currently for development route

--- src/authenticationModule.js
+++ src/authenticationModule.js
@@ -1,14 +1,16 @@
 const authModule = (sendRequest) => {
+  const basePath = '/server/development/auth';
+
   const register = (email, password) => {
-    return sendRequest('/register', 'POST', { email, password });
+    return sendRequest(`${basePath}/signup`, 'POST', { email, password });
   };
   
   const login = (email, password) => {
-    return sendRequest('/login', 'POST', { email, password });
+    return sendRequest(`${basePath}/login`, 'POST', { email, password });
   };
 
   const logout = (email) => {
-    return sendRequest('/logout', 'POST', { email });
+    return sendRequest(`${basePath}/logout`, 'POST', { email });
   };
 
   return {
